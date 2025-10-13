4e58cb1e61a8ec4abf349b8e92033084980ba406
Florian Bernd <mail@flobernd.de>
2025-10-13 17:14:51 +0200

    Add `AsFunctionTool` example (#2)

---
+++ dotnet/AsFunctionTool/ECommerceQuery.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Elastic.Clients.Elasticsearch;
+
+internal sealed class ECommerceQuery
+{
+    private readonly ElasticsearchClient _client;
+
+    public ECommerceQuery(IElasticsearchClientSettings settings)
+    {
+        ArgumentNullException.ThrowIfNull(settings);
+
+        this._client = new ElasticsearchClient(settings);
+    }
+
+    [Description("Get the customer information for a given name.")]
+    public async Task<IEnumerable<JsonElement>> QueryCustomersAsync(
+        [Description("The name of the customer to find.")]
+        string name,
+        [Description("The maximum number of results to return.")]
+        int limit = 10)
+    {
+        IEnumerable<JsonElement> response = await this._client.Esql
+            .QueryAsObjectsAsync<JsonElement>(x => x
+                .Params(
+                    name,
+                    limit
+                )
+                .Query("""
+                       FROM kibana_sample_data_ecommerce
+                       | WHERE MATCH(customer_full_name, ?1, {"operator": "AND"})
+                       | LIMIT ?2
+                       """
+                )
+            )
+            .ConfigureAwait(false);
+
+        Console.WriteLine($"-- DEBUG - Tool: QueryCustomersAsync, ES|QL query parameters: name = {name}, limit = {limit}");
+
+        return response;
+    }
+
+    [Description("Get the total revenue grouped by city.")]
+    public async Task<IEnumerable<JsonElement>> QueryRevenueAsync()
+    {
+        IEnumerable<JsonElement> response = await this._client.Esql
+            .QueryAsObjectsAsync<JsonElement>(x => x
+                .Query("""
+                       FROM kibana_sample_data_ecommerce
+                       | STATS revenue = SUM(taxful_total_price) BY geoip.city_name
+                       | SORT revenue DESC
+                       | LIMIT 1000
+                       """)
+            )
+            .ConfigureAwait(false);
+
+        Console.WriteLine("-- DEBUG - Tool: QueryRevenueAsync");
+
+        return response;
+    }
+}
---
+++ dotnet/AsFunctionTool/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ClientModel;
+using Azure.AI.OpenAI;
+using Elastic.Clients.Elasticsearch;
+using Elastic.Transport;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using OpenAI;
+
+var oaiEndpoint = 
+    Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT") 
+    ?? throw new InvalidOperationException("AZURE_OPENAI_ENDPOINT is not set.");
+var oaiDeploymentName = 
+    Environment.GetEnvironmentVariable("AZURE_OPENAI_RESPONSES_DEPLOYMENT_NAME") 
+    ?? throw new InvalidOperationException("AZURE_OPENAI_RESPONSES_DEPLOYMENT_NAME is not set.");
+var oaiKey = 
+    Environment.GetEnvironmentVariable("AZURE_OPENAI_KEY") 
+    ?? throw new InvalidOperationException("AZURE_OPENAI_KEY is not set.");
+
+var esEndpoint =
+    Environment.GetEnvironmentVariable("ELASTICSEARCH_ENDPOINT") 
+    ?? throw new InvalidOperationException("ELASTICSEARCH_ENDPOINT is not set.");
+var esKey =
+    Environment.GetEnvironmentVariable("ELASTICSEARCH_API_KEY") 
+    ?? throw new InvalidOperationException("ELASTICSEARCH_API_KEY is not set.");
+
+var eCommercePlugin =
+    new ECommerceQuery(
+        new ElasticsearchClientSettings(new SingleNodePool(new Uri(esEndpoint)))
+            .Authentication(new ApiKey(esKey))
+            .EnableDebugMode()
+    );
+
+// Create the chat client and agent, and provide the function tool to the agent.
+var ecommerceAgent = new AzureOpenAIClient(
+        new Uri(oaiEndpoint), 
+        new ApiKeyCredential(oaiKey))
+    .GetChatClient(oaiDeploymentName)
+    .CreateAIAgent(
+        instructions: "You are a helpful assistant for an ecommerce backend application.",
+        name: "ECommerceAgent",
+        description: "An agent that answers questions about orders in an ecommerce system.",
+        tools: 
+        [
+            AIFunctionFactory.Create(eCommercePlugin.QueryCustomersAsync),
+            AIFunctionFactory.Create(eCommercePlugin.QueryRevenueAsync)
+        ]
+    );
+
+// Create the main agent, and provide the ecommerce agent as a function tool.
+var agent = new AzureOpenAIClient(
+        new Uri(oaiEndpoint),
+        new ApiKeyCredential(oaiKey))
+    .GetChatClient(oaiDeploymentName)
+    .CreateAIAgent("You are a helpful assistant who responds in German.", tools: [ecommerceAgent.AsAIFunction()]);
+
+// Invoke the agent and output the text result.
+
+Console.WriteLine(await agent.RunAsync("Is Eddie Underwood our customer? If so, what is his email?"));
+Console.WriteLine(await agent.RunAsync("List all customers with the last name 'Smith'. Limit to 5 results."));
+Console.WriteLine(await agent.RunAsync("What are the first three cities with the highest revenue?"));
